2aaa332fec4e5707d5530fe9b6000291a3c500fa
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2024-10-12 21:09:23 -0500

    middleware auth  todas las rutas creadas

--- sisven/routes/web.php
+++ sisven/routes/web.php
@@ -15,6 +15,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index'); // Mostrar todas las categorías
+    Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create'); // Formulario para crear nueva categoría
+    Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store'); // Guardar la nueva categoría
+    Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit'); // Formulario para editar categoría
+    Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update'); // Actualizar la categoría
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy'); // Eliminar una categoría
 });
+
 
 require __DIR__.'/auth.php';
